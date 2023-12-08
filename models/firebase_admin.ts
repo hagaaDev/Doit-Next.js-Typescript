56c58aaaf63ad4f66982e7b89f8192d2169cbc98
sunheeK <sunheekim@kaltour.com>
2023-12-08 16:36:41 +0900

    파이어베이스 연결

---
+++ models/firebase_admin.ts
@@ -0,0 +1,59 @@
+import * as admin from 'firebase-admin';
+
+interface Config {
+  credential: {
+    privateKey: string;
+    clientEmail: string;
+    projectId: string;
+  };
+}
+
+export default class FirebaseAdmin {
+  public static instance: FirebaseAdmin;
+
+  private init = false;
+
+  public static getInstance(): FirebaseAdmin {
+    if (FirebaseAdmin.instance === undefined || FirebaseAdmin.instance === null) {
+      // 초기화 진행
+      FirebaseAdmin.instance = new FirebaseAdmin();
+      // Todo: 환경을 초기화한다.
+      FirebaseAdmin.instance.bootstrap();
+    }
+    return FirebaseAdmin.instance;
+  }
+
+  /* 환경을 초기화 할 때 사용하는 메서드 */
+  private bootstrap(): void {
+    const haveApp = admin.apps.length !== 0;
+    if (haveApp) {
+      this.init = true;
+      return;
+    }
+
+    const config: Config = {
+      credential: {
+        projectId: process.env.projectId || '',
+        clientEmail: process.env.clientEmail || '',
+        privateKey: (process.env.privateKey || '').replace(/\\n/g, '\n'),
+      },
+    };
+    admin.initializeApp({ credential: admin.credential.cert(config.credential) });
+    console.info('bootstrap firebase admin');
+  }
+
+  /* firestore를 반환 */
+  public get Firebase(): FirebaseFirestore.Firestore {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.firestore();
+  }
+
+  public get Auth(): admin.auth.Auth {
+    if (this.init === false) {
+      this.bootstrap();
+    }
+    return admin.auth();
+  }
+}
